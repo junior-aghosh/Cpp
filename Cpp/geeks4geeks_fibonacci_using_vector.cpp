74b59bdeec384bfe1c77a28e4f8c4e869202e3e8
junior-aghosh <mail2ankurghosh@gmail.com>
2022-07-31 19:01:37 +0530

    init

---
+++ Cpp/geeks4geeks_fibonacci_using_vector.cpp
@@ -0,0 +1,46 @@
+/*
+Given a number N, find the first N Fibonacci numbers. The first two number of the series are 1 and 1.
+
+Example 1:
+
+Input:
+N = 5
+Output: 1 1 2 3 5
+
+Your task is to complete printFibb() which takes single argument N and returns a list of first N Fibonacci numbers.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints: 1<= N <=84
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<long long> printf(int n){
+    vector<long long> temp;
+    temp.push_back(1);
+    if (n<2) return temp;
+    else{
+        temp.push_back(1);
+        int i=0;
+        while(n != temp.size()){
+            temp.push_back(temp[i]+temp[i+1]);
+            i=i+1;
+        }
+        return temp;
+    }
+}
+
+int main(){
+    int N;
+    cout << "Enter N: ";
+    cin >> N;
+    vector<long long> result = printf(N);
+    for(auto element:result){
+        cout << element << " ";
+    }
+    return 0;
+}
