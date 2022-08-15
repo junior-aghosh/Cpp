45a3ada7dc6a351b750292f098e01abb818ab010
junior-aghosh <mail2ankurghosh@gmail.com>
2022-08-15 18:28:28 +0530

    Updated string split

--- Cpp/String_split_vector.cpp
+++ Cpp/String_split_vector.cpp
@@ -4,24 +4,20 @@
 #include<string>
 using namespace std;
 
-/*string =  "how do i split in vector"
-
-
-*/
 void split_string(string &line, vector<string> &words_v, char &delim){
     stringstream S(line);
     string temp = "";
-    for(char &c:line){
-        if(c == delim){
+    for(char &c:line){    // ranged based loop to iterate over characters of string
+        if(c == delim){    // if space encountered push the temp string in vector and clear the temp
             words_v.push_back(temp);
             temp="";
             continue;
         }
         else{
-            temp.push_back(c);
+            temp = temp + c;  // concatenate characters in temp string till the word is formed
         }
-        
     }
+    words_v.push_back(temp); // this is to store the last word of the sentence in vector as the condition only checks for space
 }
 
 int main(){
