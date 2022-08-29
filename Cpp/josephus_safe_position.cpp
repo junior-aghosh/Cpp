a2f1144f9809698f7fa521cfc8b6f8fc802a7631
junior-aghosh <mail2ankurghosh@gmail.com>
2022-08-29 10:46:36 +0530

    josephus_safe_position

---
+++ Cpp/josephus_safe_position.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int safeposition(int &n, int &k){
+    
+}
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    cout << safeposition(n,k);
+
+    return 0;
+}
